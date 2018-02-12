2919b420ed0f255736d217953c5428b646fd5fd4
Zach <zdr747@me.com>
2018-02-12 15:45:43 -0500

    Remove Attendance List Creation from TeamController

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -10,25 +10,8 @@ class TeamController extends Controller
 {
     public function index(Team $team)
     {
+        
     	$games = $team->games();
-
-    	$players = $team->players;
-
-    	foreach ($games as $game => $game_info) :
-
-    		foreach ($players as $player => $player_info) :
-    			
-    			AttendanceList::create([
- 
-    			    'team_id' => $team->id,
-    			    'user_id' => $player_info['id'],
-    			    'game_id' => $game_info['id']
-
-    			]);
-
-    		endforeach;
-
-    	endforeach;
 
     	return view('team.index', compact('games'));
 
